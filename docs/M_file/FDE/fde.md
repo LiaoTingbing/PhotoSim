b75c928d98903601580583ddf0ef2773397d26fa
ltb <ltb@DESKTOP-FO8H3TR>
2025-11-08 04:22:13 +0800

    docs(FDE): 更新FDE文档和图片资源
    删除旧的示例文件和图片资源，添加新的FDE文档和模式图片

---
+++ docs/M_file/FDE/fde.md
@@ -0,0 +1,61 @@
+---
+sidebar_position: 4
+---
+
+# FDE 2D 案例
+
+## 矩形波导
+
+计算的分量：Ex Ey Ez Hx Hy Hz
+
+对对角介电常数和对角磁导率
+
+#### 矩形波导参数
+```matlab
+%% 全局参数
+lx = 8;
+ly = 6;
+dx = 0.1;
+dy = 0.1;
+lambda = 1.55;
+w = 3;
+h = 2;
+index = 1.8;
+backIndex = 1.1;
+nmodes = 20;
+nmodes_max = 100;
+toler = 1e-6;
+```
+
+### 结构
+
+![alt text](matlab/images/fde/structure.png)
+
+### 有效折射率
+
+```matlab
+1.7556826063901874e+00
+1.7510039383666316e+00
+1.7078231749348238e+00
+1.7051830771757968e+00
+1.6702098530713665e+00
+1.6436053137711506e+00
+1.6346904714717176e+00
+1.6191671395326253e+00
+1.6165329314886545e+00
+1.5972754141960221e+00
+1.5326418063207210e+00
+1.5221029861551907e+00
+1.5207756370334444e+00
+1.5205899123715299e+00
+1.4925200271322971e+00
+1.4627175584545149e+00
+1.4577585555870196e+00
+1.4090636582240483e+00
+1.4058159208264256e+00
+1.3996551948712739e+00
+```
+
+### 模场分布
+
+![alt text](matlab/images/fde/Mode1.png) ![alt text](matlab/images/fde/Mode2.png) ![alt text](matlab/images/fde/Mode3.png) ![alt text](matlab/images/fde/Mode4.png) ![alt text](matlab/images/fde/Mode5.png) ![alt text](matlab/images/fde/Mode6.png) ![alt text](matlab/images/fde/Mode7.png) ![alt text](matlab/images/fde/Mode8.png) ![alt text](matlab/images/fde/Mode9.png) ![alt text](matlab/images/fde/Mode10.png) ![alt text](matlab/images/fde/Mode11.png) ![alt text](matlab/images/fde/Mode12.png) ![alt text](matlab/images/fde/Mode13.png) ![alt text](matlab/images/fde/Mode14.png) ![alt text](matlab/images/fde/Mode15.png) ![alt text](matlab/images/fde/Mode16.png) ![alt text](matlab/images/fde/Mode17.png) ![alt text](matlab/images/fde/Mode18.png) ![alt text](matlab/images/fde/Mode19.png) ![alt text](matlab/images/fde/Mode20.png) [alt text](fde.md) 
---
+++ docs/M_file/FDE/matlab/fde.m
@@ -0,0 +1,259 @@
+%% 物理参数
+EPS0 = 8.85e-12;
+MU0 = pi * 4e-7;
+C0 = 1 / sqrt(MU0*EPS0);
+Z0 = sqrt(MU0/EPS0);
+
+%% 全局参数
+lx = 8;
+ly = 6;
+dx = 0.1;
+dy = 0.1;
+lambda = 1.55;
+w = 3;
+h = 2;
+index = 1.8;
+backIndex = 1.1;
+nmodes = 20;
+nmodes_max = 100;
+toler = 1e-6;
+%% 参数计算
+
+k0 = 2 * pi / lambda;
+
+cx = round(lx/dx);
+cy = round(ly/dy);
+nx = cx + 1;
+ny = cy + 1;
+nt = nx * ny;
+I = ones(nt, 1);
+
+%% 建模
+
+x = (-lx / 2:dx:lx / 2)';
+y = (-ly / 2:dy:ly / 2)';
+xm = x + dx / 2;
+ym = y + dy / 2;
+
+s1 = [dsearchn(x, -w/2), dsearchn(x, w/2), dsearchn(y, -h/2), dsearchn(y, h/2)]';
+
+eps_x = backIndex * backIndex * ones(nx, ny);
+eps_y = backIndex * backIndex * ones(nx, ny);
+eps_z = backIndex * backIndex * ones(nx, ny);
+
+eps_x(s1(1):s1(2)-1, s1(3):s1(4)) = index^2;
+eps_y(s1(1):s1(2), s1(3):s1(4)-1) = index^2;
+eps_z(s1(1):s1(2), s1(3):s1(4)) = index^2;
+
+% eps_x(s1(1):s1(2)-1, s1(3)) = 0.5 * (backIndex^2 + index^2);
+% eps_x(s1(1):s1(2)-1, s1(4)) = 0.5 * (backIndex^2 + index^2);
+%
+% eps_y(s1(1), s1(3):s1(4)-1) = 0.5 * (backIndex^2 + index^2);
+% eps_y(s1(2), s1(3):s1(4)-1) = 0.5 * (backIndex^2 + index^2);
+%
+% eps_z(s1(1):s1(2), s1(3)) = 0.5 * (backIndex^2 + index^2);
+% eps_z(s1(1):s1(2), s1(4)) = 0.5 * (backIndex^2 + index^2);
+% eps_z(s1(1), s1(3):s1(4)) = 0.5 * (backIndex^2 + index^2);
+% eps_z(s1(2), s1(3):s1(4)) = 0.5 * (backIndex^2 + index^2);
+%
+% eps_z(s1(1), s1(3)) = 0.25 * (3 * backIndex^2 + index^2);
+% eps_z(s1(1), s1(4)) = 0.25 * (3 * backIndex^2 + index^2);
+% eps_z(s1(2), s1(3)) = 0.25 * (3 * backIndex^2 + index^2);
+% eps_z(s1(2), s1(4)) = 0.25 * (3 * backIndex^2 + index^2);
+%% 构建算子
+
+UX = spdiags([-I, I]/dx, [0, 1], nt, nt);
+for i = nx:nx:nt
+    UX(i, :) = UX(i-1, :);
+end
+
+UY = spdiags([-I, I]/dy, [0, nx], nt, nt);
+UY(nt-nx+1:nt, :) = UY(nt-2*nx+1:nt-2*nx+nx, :);
+
+VX = spdiags([-I, I]/dx, [-1, 0], nt, nt);
+for i = 1:nx:nt
+    VX(i, :) = 0;
+end
+
+VY = spdiags([-I, I]/dy, [-nx, 0], nt, nt);
+VY(1:nx, :) = 0;
+%% 构建矩阵
+
+EPS_X = spdiags(eps_x(:), 0, nt, nt);
+EPS_Y = spdiags(eps_y(:), 0, nt, nt);
+EPS_Z = spdiags(eps_z(:), 0, nt, nt);
+
+MU_X = speye(nt);
+MU_Y = speye(nt);
+MU_Z = speye(nt);
+
+Pxx = -VX / k0 * inv(MU_Z) * UY;
+Pxy = VX / k0 * inv(MU_Z) * UX + k0 * EPS_Y;
+Pyx = -VY / k0 * inv(MU_Z) * UY - k0 * EPS_X;
+Pyy = VY / k0 * inv(MU_Z) * UX;
+
+P = [Pxx, Pxy; Pyx, Pyy];
+
+Qxx = -UX / k0 * inv(EPS_Z) * VY;
+Qxy = UX / k0 * inv(EPS_Z) * VX + k0 * MU_Y;
+Qyx = -UY / k0 * inv(EPS_Z) * VY - k0 * MU_X;
+Qyy = UY / k0 * inv(EPS_Z) * VX;
+
+Q = [Qxx, Qxy; Qyx, Qyy];
+
+[et_, d] = eigs(-Q*P, nmodes_max, index*index*k0*k0);
+neff_ = sqrt(diag(d)) / k0;
+%% 删除特征值
+
+flag = ones(size(neff_));
+for i = 2:length(neff_) - 1
+    if neff_(i) - neff_(i+1) < toler || neff_(i-1) - neff_(i) < toler
+        flag(i) = 0;
+    end
+end
+neff = neff_(flag > 0);
+et = et_(:, flag > 0);
+
+neff = neff(1:nmodes);
+et = et(:, 1:nmodes);
+%% 计算场分量
+
+ex = et(1:nt, :);
+ey = et(nt+1:2*nt, :);
+
+for midx = 1:nmodes
+    beta = neff(midx) * k0;
+    hx(:, midx) = -1i / beta * (Pxx * ex(:, midx) + Pxy * ey(:, midx));
+    hy(:, midx) = -1i / beta * (Pyx * ex(:, midx) + Pyy * ey(:, midx));
+end
+
+ez = 1 / k0 * inv(EPS_Z) * (VX * hy - VY * hx);
+hz = 1 / k0 * inv(MU_Z) * (UX * ey - UY * ex);
+
+F.Ex = reshape(ex, nx, ny, nmodes);
+F.Ey = reshape(ey, nx, ny, nmodes);
+F.Ez = reshape(ez, nx, ny, nmodes);
+F.Hx = reshape(hx, nx, ny, nmodes) * 1i / Z0;
+F.Hy = reshape(hy, nx, ny, nmodes) * 1i / Z0;
+F.Hz = reshape(hz, nx, ny, nmodes) * 1i / Z0;
+
+%% 绘图
+fp = "images/fde/";
+mkdir(fp);
+
+for midx = 1:nmodes
+
+    subplot(231)
+    pcolor(x, y, abs(F.Ex(:, :, midx))');
+    xlabel("X (m)")
+    ylabel("Y (m)")
+    subtitle("Ex Amplititude")
+    axis equal
+    shading interp
+    colormap jet
+    colorbar
+
+    subplot(232)
+    pcolor(x, y, abs(F.Ey(:, :, midx))');
+    xlabel("X (m)")
+    ylabel("Y (m)")
+    subtitle("Ey Amplititude")
+    axis equal
+    shading interp
+    colormap jet
+    colorbar
+
+    subplot(233)
+    pcolor(x, y, abs(F.Ez(:, :, midx))');
+    xlabel("X (m)")
+    ylabel("Y (m)")
+    subtitle("Ez Amplititude")
+    axis equal
+    shading interp
+    colormap jet
+    colorbar
+
+    subplot(234)
+    pcolor(x, y, abs(F.Hx(:, :, midx))');
+    xlabel("X (m)")
+    ylabel("Y (m)")
+    subtitle("Hx Amplititude")
+    axis equal
+    shading interp
+    colormap jet
+    colorbar
+
+    subplot(235)
+    pcolor(x, y, abs(F.Hy(:, :, midx))');
+    xlabel("X (m)")
+    ylabel("Y (m)")
+    subtitle("Hy Amplititude")
+    axis equal
+    shading interp
+    colormap jet
+    colorbar
+
+    subplot(236)
+    pcolor(x, y, abs(F.Hz(:, :, midx))');
+    xlabel("X (m)")
+    ylabel("Y (m)")
+    subtitle("Hz Amplititude")
+    axis equal
+    shading interp
+    colormap jet
+    colorbar
+
+
+    sgtitle("Mode #"+num2str(midx)+"="+num2str(neff(midx)))
+
+    set(gcf ,"OuterPosition",get(0, "ScreenSize"))
+
+    exportgraphics(gcf , fp + "Mode" + num2str(midx) + ".png")
+
+    %
+    % drawnow
+    %
+    % pause(0.1)
+
+end
+
+%% 结构
+close
+
+subplot(221)
+pcolor(x, y, eps_x');
+xlabel("X (m)")
+ylabel("Y (m)")
+subtitle("eps x")
+axis equal
+shading interp
+colormap jet
+colorbar
+
+subplot(222)
+pcolor(x, y, eps_y');
+xlabel("X (m)")
+ylabel("Y (m)")
+subtitle("eps y")
+axis equal
+shading interp
+colormap jet
+colorbar
+
+subplot(223)
+pcolor(x, y, eps_z');
+xlabel("X (m)")
+ylabel("Y (m)")
+subtitle("eps z")
+axis equal
+shading interp
+colormap jet
+colorbar
+
+sgtitle("相对介电常数分布")
+
+set(gcf , "outerposition" , get(0,"ScreenSize"))
+
+exportgraphics(gcf , fp + "structure.png")
+
+save(fp + "neff.txt" , "neff","-ascii" ,"-double");
