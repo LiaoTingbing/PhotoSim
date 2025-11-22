e70425946f58d4ef86067a48b66283d3aced6a79
ltb <ltb@DESKTOP-FO8H3TR>
2025-11-22 14:21:06 +0800

    chore: 更新项目名称和基础URL路径
    将项目名称从PhotoSim更改为PhotoSimLab，并相应调整基础URL路径以匹配新的项目名称

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,12 +26,12 @@ const config = {
   url: 'https://liaotingbing.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/PhotoSim/',
+  baseUrl: '/PhotoSimLab/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'LiaoTingbing', // Usually your GitHub org/user name.
-  projectName: 'PhotoSim', // Usually your repo name.
+  projectName: 'PhotoSimLab', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
